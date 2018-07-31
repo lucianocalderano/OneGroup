957f12c904682d565f8e3ffae076f366c0355039
Luciano <luciano.calderano@mobilesoft.it>
2018-07-31 16:48:25 +0200

    login view

--- OneGroup/ViewController.swift
+++ OneGroup/ViewController.swift
@@ -9,16 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var userView: UIView!
+    @IBOutlet var passView: UIView!
+    
+    @IBOutlet var userText: UITextField!
+    @IBOutlet var passText: UITextField!
+    
+    @IBOutlet var saveCredBtn: UIButton!
+    @IBOutlet var showPassBtn: UIButton!
+    @IBOutlet var loginBtn: UIButton!
 
+    let check = UIImage.init(named: "check")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        #if DEBUG
+        userText.text = "admin@admin.it"
+        passText.text = "admin_0n3"
+        #endif
+
+        userView.layer.cornerRadius = userView.frame.height / 2
+        passView.layer.cornerRadius = passView.frame.height / 2
+        loginBtn.layer.cornerRadius = 5
+        
+        saveCredBtn.layer.borderColor = UIColor.white.cgColor
+        saveCredBtn.layer.borderWidth = 2
+        showPassBtn.layer.borderColor = UIColor.white.cgColor
+        showPassBtn.layer.borderWidth = 2
+        
+        userText.text = UserDefaults.standard.string(forKey: "user")
+        passText.text = UserDefaults.standard.string(forKey: "pass")
+        
+        if ((userText.text?.count)! > 0 && (passText.text?.count)! > 0) {
+            selectButton(btn: saveCredBtn)
+        }
+    }
+    
+    @IBAction func saveSelected () {
+        selectButton(btn: saveCredBtn)
+    }
+    @IBAction func showSelected () {
+        selectButton(btn: showPassBtn)
+        passText.isSecureTextEntry = showPassBtn.tag == 0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginSelected () {
     }
+    
+    func selectButton(btn: UIButton){
+        if (btn.tag > 0) {
+            btn.tag = 0
+            btn.setBackgroundImage(UIImage(), for: .normal)
+        } else {
+            btn.tag = 1
+            btn.setBackgroundImage(check, for: .normal)
+        }
+    }
+    
+    func loeggedIn () {
+        if (saveCredBtn.tag > 0) {
+            UserDefaults.standard.set(userText.text, forKey: "user")
+            UserDefaults.standard.set(passText.text, forKey: "pass")
+        }
+    }
+    
 }
 
 /*
