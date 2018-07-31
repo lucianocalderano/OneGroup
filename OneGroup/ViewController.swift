0d972ed5d7b2c36a7a0ec1222d5d3d4a542dc450
Luciano <luciano.calderano@mobilesoft.it>
2018-07-31 15:17:51 +0200

    icone

--- OneGroup/ViewController.swift
+++ OneGroup/ViewController.swift
@@ -19,7 +19,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
+/*
+ grant_type:password
+ client_id:f3d259ddd3ed8ff3843839b
+ client_secret:4c7f6f8fa93d59c45502c0ae8c4a95b
+ username:admin@admin.it
+ password:admin_0n3
+ */
